001ea23554154766f262d1f90aa5277f26fca149
Jenny Cárdenas <pcardenasvm@gmail.com>
2022-03-29 12:20:16 -0500

    Add images and global style to all pages

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getAllEvents } from '../../dummy-data';
+import { getFeaturedEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
@@ -21,6 +21,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60
+  };
 }
 
 export default AllEventsPage;
